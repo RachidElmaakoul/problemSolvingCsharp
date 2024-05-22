942c05d5928af7d90c57718458cb12d91d066b30
RachidElmaakoul <146081594+RachidElmaakoul@users.noreply.github.com>
2024-05-23 00:25:17 +0100

    Add files via upload

---
+++ FirstProblem/FirstProblem1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace FirstProblem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double Tax = 0.03;
+
+            Console.Write("First Name : ");
+            var fName = Console.ReadLine();
+
+            Console.Write("Last Name : ");
+            var LName = Console.ReadLine();
+
+            Console.Write("Wage : ");
+            var Wage = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("LoggedHours : ");
+            var LoggedHoures = Convert.ToDouble(Console.ReadLine());
+
+            var NetSalary = Wage * LoggedHoures - (Wage * LoggedHoures * Tax);
+
+
+
+            Console.WriteLine("\n==============================================\n");
+
+            Console.WriteLine($"First Name   : {fName}");
+            Console.WriteLine($"Last Name    : {LName}");
+            Console.WriteLine($"Wage         : {Wage}");
+            Console.WriteLine($"LoggedHoures : {LoggedHoures}");
+            Console.WriteLine($"Nat Sallary  : {NetSalary}");
+
+
+
+
+
+        }
+    }
+}
